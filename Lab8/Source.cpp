b8a67c4226df90b4dffc58b65dafed6f8a2e1078
Igor Semikin <i.semikin@list.ru>
2022-04-06 21:10:15 +0300

    Release

--- Lab8/Source.cpp
+++ Lab8/Source.cpp
@@ -1,182 +1,5 @@
 #include <iostream>
 
-//#define TERMINAL_VALUE -1
-//
-//struct RaggedArray
-//{
-//	int** data = NULL;
-//	int rows = NULL;
-//};
-//
-//int askUserScan()
-//{
-//	int choice = 0;
-//	do {
-//		printf("Введите способ записи:\n1 - заполнить массив рандомно\n2 - заполнить массив из файла txt\n3 - заполнить массив из файла bin\nВыбор:");
-//		scanf_s("%d", &choice);
-//	} while (choice > 3 || choice < 1);
-//	return choice;
-//}
-//int askUserSave()
-//{
-//	int choice = 0;
-//	do {
-//		printf("Введите способ сохранения:\n1 - в файл txt\n2 - в файл bin\nВыбор:");
-//		scanf_s("%d", &choice);
-//	} while (choice > 2 || choice < 1);
-//	return choice;
-//}
-//
-//int scanRowsSize()
-//{
-//	int choice = 0;
-//	do {
-//		printf("Введите количество строк [0;1000]:\n");
-//		scanf_s("%d", &choice);
-//	} while (choice > 1000 || choice < 0);
-//	return choice;
-//}
-//int scanColsSize()
-//{
-//	int choice = 0;
-//	static int counter = 0;
-//	counter++;
-//	do {
-//		printf("№%d - ", counter);
-//		scanf_s("%d", &choice);
-//	} while (choice > 1000 || choice < 0);
-//	return choice;
-//}
-//
-//int** allocateMas(RaggedArray mas)
-//{
-//	mas.data = (int**)malloc(sizeof(int*) * mas.rows);
-//	printf("Введите количество столбцов [0;1000]:\n");
-//	for (int i = 0; i < mas.rows; i++)
-//	{
-//		int cols = scanColsSize();
-//		mas.data[i] = (int*)malloc(sizeof(int) * (cols + 1));
-//		for (int j = 0; j < cols; j++)
-//			mas.data[i][j] = 0;
-//		mas.data[i][cols] = TERMINAL_VALUE;
-//	}
-//	return mas.data;
-//}
-//
-//void fillMasRandom(RaggedArray mas)
-//{
-//	for (int i = 0; i < mas.rows; i++)
-//		for (int j = 0; mas.data[i][j] != TERMINAL_VALUE; j++)
-//			mas.data[i][j] = rand() % 100;
-//}
-//
-//void outputMasToScreen(RaggedArray mas)
-//{
-//	printf("\n");
-//	for (int i = 0; i < mas.rows; i++)
-//	{
-//		for (int j = 0; mas.data[i][j] != TERMINAL_VALUE; j++)
-//			printf("%5d", mas.data[i][j]);
-//		printf("\n");
-//	}
-//	printf("\n");
-//}
-//
-//void fillMasFromTxtFile(const char* filename, RaggedArray mas)
-//{
-//	int cols;
-//	FILE* f;
-//	if (fopen_s(&f, filename, "r") != 0)
-//		exit(1);
-//
-//	fscanf_s(f, "%d", &mas.rows);
-//	mas.data = (int**)malloc(sizeof(int*) * mas.rows);
-//	for (int i = 0; i < mas.rows; i++)
-//	{
-//		fscanf_s(f, "%d", &cols);
-//		mas.data[i] = (int*)malloc(sizeof(int) * (cols + 1));
-//		mas.data[i][cols] = TERMINAL_VALUE;
-//	}
-//	for (int i = 0; i < mas.rows; i++)
-//		for (int j = 0; mas.data[i][j] != TERMINAL_VALUE; j++)
-//			fscanf_s(f, "%d", &mas.data[i][j]);
-//
-//	outputMasToScreen(mas);
-//	
-//	fclose(f);
-//}
-//
-////void fillMasFromBinFile(const char* filename, RaggedArray mas, int& cols)
-////{
-////
-////}
-//
-//
-////void writeMasToTxtFile(const char* filename, RaggedArray mas)
-////{
-////	FILE* f;
-////	if (fopen_s(&f, filename, "w") != 0)
-////		exit(1);
-////
-////	fprintf_s(f, "%d ", cols);
-////
-////	for (int i = 0; i < mas.rows; i++)
-////		for (int j = 0; mas.data[i][j] != TERMINAL_VALUE; j++)
-////			fscanf_s(f, "%d", &mas.data[i][j]);
-////
-////	fclose(f);
-////}
-//
-//void freeMas(RaggedArray mas)
-//{
-//	for (int i = 0; i < mas.rows; i++)
-//		free(mas.data[i]);
-//	free(mas.data);
-//}
-//
-//int main()
-//{
-//	//3. Массив представляется указателем на вектор указателей на строки.Количество строк определяется терминальным символом.
-//	//Количество элементов строки определяется терминальным символом. Память выделяется одним блоком.
-//	system("chcp 1251");
-//	srand(time(0));
-//
-//	RaggedArray mas;
-//
-//	switch (askUserScan())
-//	{
-//	case 1:
-//	{
-//		mas.rows = scanRowsSize();
-//		mas.data = allocateMas(mas);
-//		fillMasRandom(mas);
-//		outputMasToScreen(mas);
-//		break;
-//	}
-//	case 2:
-//		fillMasFromTxtFile("1.txt", mas);
-//		break;
-//	case 3:
-//		break;
-//	}
-//
-//	switch (askUserSave())
-//	{
-//	case 1:
-//	{
-//		//writeMasToTxtFile("1.txt", mas);
-//		break;
-//	}
-//	case 2:
-//
-//		break;
-//	}
-//
-//	freeMas(mas);
-//
-//	return 0;
-//}
-
 /*Задание:
 Массив представляется указателем на вектор указателей на строки.
 Количество строк определяется терминальным символом.
@@ -202,15 +25,35 @@ int askUserSave()
 	return choice;
 }
 
-float** mallocArray(float** mas, int rows)
+float** mallocArray(float** mas)
 {
-	mas = (float**)malloc(sizeof(float*) * (rows + 1));
+	mas = (float**)malloc(sizeof(float*));
+	if (mas == NULL)
+	{
+		printf("Неудалось выделить память!");
+		exit(1);
+	}
+	return mas;
+}
+float** reallocArray(float** mas, int rows)
+{
+	mas = (float**)realloc(mas, sizeof(float*) * (rows + 1));
+	if (mas == NULL)
+	{
+		printf("Неудалось выделить память!");
+		exit(1);
+	}
 	mas[rows] = NULL;
 	return mas;
 }
 void mallocArrayForArray(float** mas, int rows, int cols)
 {
 	mas[rows] = (float*)malloc(sizeof(float) * (cols + 1));
+	if (mas[rows] == NULL)
+	{
+		printf("Неудалось выделить память!");
+		exit(1);
+	}
 	mas[rows][cols] = NULL;
 }
 void freeArray(float** mas)
@@ -250,12 +93,13 @@ void printArray(float** mas)
 			printf("%6.1f", mas[rows][col]);
 		printf("\n");
 	}
+	printf("\n");
 }
 
 void fillArrayRandomly(float** mas)
 {
 	int cols;
-	for (int rows = 0;mas[rows]!=NULL;rows++)
+	for (int rows = 0; mas[rows] != NULL; rows++)
 	{
 		cols = scanColsSize();	
 		mallocArrayForArray(mas, rows, cols);
@@ -265,29 +109,141 @@ void fillArrayRandomly(float** mas)
 	}
 }
 
+float** fillArrayFromTxtFile(float** mas, char* filename)
+{
+	FILE* f;
+	if (fopen_s(&f, filename, "r") != NULL)
+		exit(1);
+
+	int rows = 0, cols = 0;
+	int pos = ftell(f);
+	while (!feof(f))
+	{
+		char currentChar = fgetc(f);
+		if (currentChar == '.')
+			cols++;
+		if (currentChar == '\n')
+		{
+			mas = reallocArray(mas, rows + 1);
+			mas[rows] = (float*)malloc(sizeof(float) * (cols + 1));
+
+			fseek(f, pos, SEEK_SET);
+			for (int col = 0; col < cols; col++)
+				fscanf_s(f, "%f", &mas[rows][col]);
+			fscanf_s(f, "\n");
+			pos = ftell(f);
+
+			mas[rows][cols] = NULL;
+			cols = 0;
+			rows++;
+		}
+	}
+	fclose(f);
+
+	return mas;
+}
+
+float** fillArrayFromBinFile(float** mas, char* filename)
+{
+	FILE* f;
+	if (fopen_s(&f, filename, "rb") != NULL)
+		exit(1);
+
+	int rows = 0, cols = 0;
+	mas = reallocArray(mas, rows);
+	float value;
+	while (!feof(f))
+	{
+		fread(&value, sizeof(float), 1, f);
+		if (value == NULL)
+		{
+			rows++;
+			mas = reallocArray(mas, rows);
+			cols = 0;
+		}
+		else
+		{
+			mas[rows] = (float*)realloc(mas[rows], sizeof(float) * (cols + 2));
+			mas[rows][cols++] = value;
+			mas[rows][cols] = NULL;
+		}
+	}
+	return mas;
+}
+
+void writeArrayToTxtFile(float** mas, char* filename)
+{
+	FILE* f;
+	if (fopen_s(&f, filename, "w") != NULL)
+		exit(1);
+
+	for (int row = 0; mas[row] != NULL; row++)
+	{
+		for (int col = 0; mas[row][col] != NULL; col++)
+			fprintf(f, "%6.1f", mas[row][col]);
+		fprintf(f, "\n");
+	}
+}
+
+void writeArrayToBinFile(float** mas, char* filename)
+{
+	FILE* f;
+	if (fopen_s(&f, filename, "wb") != NULL)
+		exit(1);
+	
+	for (int row = 0; mas[row] != NULL; row++)
+	{
+		int col = 0;
+		while (mas[row][col] != NULL)
+			fwrite(&mas[row][col++], sizeof(float), 1, f);
+		fwrite(&mas[row][col], sizeof(float), 1, f);
+	}
+}
+
 int main()
 {
 	system("chcp 1251"); system("cls");
 	srand(time(NULL));
 
 	float** mas = NULL;
+	mas = mallocArray(mas);
+	char filenameTxt[] = "1.txt";
+	char filenameBin[] = "2.bin";
 
 	switch (askUserScan())
 	{
 	case 1:
 	{
 		int rows = scanRowsSize();
-		mas = mallocArray(mas, rows);
+		mas = reallocArray(mas, rows);
 		fillArrayRandomly(mas);
-
+		break;
+	}
+	case 2:
+	{
+		mas = fillArrayFromTxtFile(mas, filenameTxt);
+		break;
+	}
+	case 3:
+	{
+		mas = fillArrayFromBinFile(mas, filenameBin);
 		break;
 	}
 	}
 
 	printArray(mas);
 
+	switch (askUserSave())
+	{
+	case 1:
+		writeArrayToTxtFile(mas, filenameTxt);
+		break;
+	case 2:
+		writeArrayToBinFile(mas, filenameBin);
+		break;
+	}
 
+	freeArray(mas);
 
 	return 0;
 }
-
